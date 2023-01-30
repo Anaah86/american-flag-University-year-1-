b64479eb6b0e7a079d5cc5a381071cddc7b0d046
Anaah86 <anathfarook@gmail.com>
2023-01-30 17:40:11 +0000

    Add files via upload

---
+++ Ameicanflag/src/ameicanflag/Ameicanflag.java
@@ -0,0 +1,26 @@
+
+ //This program will output an American flag
+ //Name : Anaah Mohamed Farook
+ 
+
+package ameicanflag;
+
+
+public class Ameicanflag {  //class
+
+    
+    public static void main(String[] args) { //main method
+       
+        for (int i=1; i<=3; i++) { // This for loop allows to repeat the string up to 3 times and incremented by one
+            System.out.println("* * * * * * ===========================\n * * * * *  ===========================");
+            
+        }
+       
+        for (int i=1; i<=7; i++){ // This for loop allows to repeat the string up to 7 times and incremented by one
+        System.out.println("=======================================");
+        }
+    }
+    
+    
+    
+}
